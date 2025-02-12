0071ba7185b1fe9a8253713ff76fccd81150ae26
Farrukh4ik <allaberganovfarrukh@gmail.com>
2025-02-12 18:57:52 +0500

    My homework

---
+++ pl.py
@@ -0,0 +1,8 @@
+#Slice List: Create a new list that contains only the first three elements of the original list. List8
+
+list1 =  ['apple', 'orange', 'pineapple', 'banana']
+
+
+new_list = (list1[:3])
+
+print(new_list)
